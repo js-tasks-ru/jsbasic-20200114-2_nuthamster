fa10713a87789dd163d1a343c142e7b228c99abb
Zimava <60003797+Zimava@users.noreply.github.com>
2020-01-21 15:07:11 +0300

    Вычисление факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,4 +5,17 @@
  */
 function factorial(n) {
 
+	let result = 1;
+
+  if (n===0) {
+	  return result}; //исключение для n=0
+
+	if (n===1){
+	  return result}; //исключение для n=1
+
+	for (let i=1; i<=n; i++) {
+	  result = result * i;
+	}
+	return result;
+
 }
