db9f9d11d40b663256a54925c59d4faa04f4c557
Zimava <60003797+Zimava@users.noreply.github.com>
2020-02-05 20:44:46 +0300

    Имена

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,5 +2,8 @@
  * @param   {{ name: string, age: number }[]} users
  * @returns {string[]}  объект
  */
+'use strict';
 function namify(users) {
+	let names = users.map(user=>user.name);
+	return names;
 }
