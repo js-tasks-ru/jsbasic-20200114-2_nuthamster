f7ef76434ad1702650e2edd1c75230f16af42e1e
Zimava <60003797+Zimava@users.noreply.github.com>
2020-02-05 20:23:55 +0300

    одноСловоЛесенкой
    :)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,5 +2,8 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-}
+  function camelize(str) {
+    let newStr = []
+    newStr = str.split('-').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
+    return newStr;
+  }
