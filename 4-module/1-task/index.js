59f4ac20edf6655102930321079aa5faa4697c76
Zimava <60003797+Zimava@users.noreply.github.com>
2020-02-17 14:13:49 +0300

    Friend list

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,7 +1,24 @@
+  
 /**
  * Генерация HTML списка друзей
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+'use strict';
+
 function makeFriendsList(friends) {
+  //create list
+  let list = document.createElement('ul');
+
+  //create item of list and add to it first name and last name
+  friends.forEach(item => {
+    let listItem = document.createElement('li');
+
+    listItem.innerHTML = `${item.firstName} ${item.lastName}`;
+
+    list.append(listItem);
+  });
+  
+  //return list 
+  return list; 
 }
