c2bab48164d84c018dded80c27580e3b26864cfa
Zimava <60003797+Zimava@users.noreply.github.com>
2020-02-17 15:10:57 +0300

    Highlights

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -2,5 +2,31 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+
+'use strict';
+
 function highlight(table) {
+
+  for (let tr of table.querySelector('tbody').rows) {
+
+    if (!tr.hasAttribute('hidden')) {
+      tr.setAttribute('hidden', '');
+    }
+
+    if (tr.cells[1].innerHTML < 18) {
+      tr.setAttribute('style', 'text-decoration: line-through');
+    }
+
+    if (tr.cells[2].innerHTML === 'm') {
+      tr.classList.add('male');
+    } else {
+      tr.classList.add('female');
+    }
+
+    if (tr.cells[3].dataset.available === 'true') {
+      tr.classList.add('available');
+    } else if (tr.cells[3].dataset.available === 'false') {
+      tr.classList.add('unavailable');
+    }
+  }
 }
