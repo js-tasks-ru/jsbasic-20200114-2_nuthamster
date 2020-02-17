4200b57ef227f3f79a71076331b8c3d753b693c1
Zimava <60003797+Zimava@users.noreply.github.com>
2020-02-17 15:49:37 +0300

    Сортировка

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -19,11 +19,55 @@ function SortableTable(items) {
    */
   this.el = document.createElement('table');
 
-  /**
-   * Метод выполняет сортировку таблицы
-   * @param {number} column - номер колонки, по которой
-   * нужно выполнить сортировку (отсчет начинается от 0)
-   * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-   */
-  this.sort = (column, desc = false) => {};
+  this.items = items;
+
+  let _itemKeys = [];
+
+  if (items.length > 0) {
+    for (let key in items[0]) {
+      _itemKeys.push(key);
+    }
+  }
+
+  this.columnNames = ['Age', 'Name', 'Salary', 'City'];
+
+  this.render = () => {
+    let innerHTML = "";
+    
+    innerHTML += '<thead><tr>';
+    for (let columnName of this.columnNames) {
+      innerHTML += `<td>${columnName}</td>`;
+    }
+    innerHTML += '</thead></tr>';
+
+    innerHTML += '<tbody>';
+    for (let item of this.items) {
+      innerHTML += `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td></tr>`;
+    }
+    innerHTML += '</tbody>';
+
+    this.el.innerHTML = innerHTML;
+  };
+
+  this.sort = (column, desc = false) => {
+    this.items.sort((a, b) => {
+      let key = _itemKeys[column];
+      let x = a[key];
+      let y = b[key];
+
+      if (!desc) {
+        if (x > y) return 1;
+        if (x == y) return 0;
+        if (x < y) return -1;
+      } else {
+        if (x < y) return 1;
+        if (x == y) return 0;
+        if (x > y) return -1;
+      }
+    });
+
+    this.render();
+  };
+
+  this.render();  
 }
